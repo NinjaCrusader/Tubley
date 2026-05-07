740b277c91b19d99849ef3a056956945d9c94668
NinjaCrusader <jeroldpemberton@gmail.com>
2026-05-07 16:33:23 -0400

    updated the handler_upload_thumbnail to add checks for jpeg or png files and handle error if not that content type.

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -49,10 +52,11 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	defer multipartFile.Close()
 
 	mediaType := multipartHeader.Header.Get("Content-Type")
-
-	imageData, err := io.ReadAll(multipartFile)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Something went wrong getting file data", err)
+	if mediaType == "" {
+		respondWithError(w, http.StatusBadRequest, "Bad Request", errors.New("The Content-Type was bad"))
+		return
+	} else if (mediaType != "image/jpeg") && (mediaType != "image/png") {
+		respondWithError(w, http.StatusBadRequest, "Bad Request", errors.New("Media type was not jpeg or png"))
 		return
 	}
 
@@ -67,16 +71,31 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	newThumbnail := thumbnail{
-		data:      imageData,
-		mediaType: mediaType,
+	//updating handler to store image file within the assets directory
+
+	imageTypeSplit := strings.Split(mediaType, "/")
+	if len(imageTypeSplit) != 2 {
+		respondWithError(w, http.StatusBadRequest, "Something went wrong", errors.New("The content type is invalid"))
+		return
 	}
 
-	videoThumbnails[video.ID] = newThumbnail
+	filenameAndExtention := videoID.String() + "." + imageTypeSplit[1]
+	newPath := filepath.Join(cfg.assetsRoot, filenameAndExtention)
+	newFile, err := os.Create(newPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong", err)
+		return
+	}
 
-	url := fmt.Sprintf("http://localhost:%v/api/thumbnails/%v", cfg.port, video.ID)
+	defer newFile.Close()
 
-	video.ThumbnailURL = &url
+	if _, err := io.Copy(newFile, multipartFile); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong", err)
+		return
+	}
+
+	pathURL := fmt.Sprintf("http://localhost:%v/assets/%v", cfg.port, filenameAndExtention)
+	video.ThumbnailURL = &pathURL
 
 	if err := cfg.db.UpdateVideo(video); err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Something went wrong updating the video url", err)
